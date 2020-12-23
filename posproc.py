585e4c74700cabc3b63c0dbf1c76ca170a5684ee
Saeid Aliei <saeidaliei2019@gmail.com>
2020-12-23 16:25:34 +0100

    add 3d limits

--- posproc.py
+++ posproc.py
@@ -1,5 +1,4 @@
 #! /usr/bin/env python3
-
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
@@ -7,23 +6,27 @@ import matplotlib.animation as animation
 
 rS = 0
 rE = 1000
-rr = 2
+rr = 50
 
 fig = plt.figure()
 axe = fig.add_subplot(111, projection="3d")
 
-
+# field = "velocities"
+field = "positions"
 pos = []
 for i in range(rS, rE, rr):
-    p = np.genfromtxt("./data/positions-{}.dat".format(i), comments="t")
+    p = np.genfromtxt("./data/{}-{}.csv".format(field,i), comments="t", delimiter=",", skip_header=1)
     pos.append(p)
 
 data = pos
 
+xmax,ymax,zmax = np.max(np.max(data, axis=1), axis=0)
+xmin,ymin,zmin = np.min(np.min(data, axis=1), axis=0)
+
 def anim_scats(iters, data, scat):
     # for i in range(data[0].shape[0]):
         # scats[i]._offsets3d = (data[iters][i,0], data[iters][i,1], data[iters][i,2])
-    scat = axe.scatter(data[iters][:,0], data[iters][:,1], data[iters][:,2], s=2, c="blue")
+    scat = axe.scatter(data[iters][:,0], data[iters][:,1], data[iters][:,2], s=0.02, c="blue")
     return scat
 
 # def update_pos(i):
@@ -37,6 +40,10 @@ def anim_scats(iters, data, scat):
 # scats = [axe.scatter(data[0][i,0], data[0][i,1], data[0][i,2]) for i in range(data[0].shape[0])]
 
 scat = axe.scatter(data[0][:,0], data[0][:,1], data[0][:,2], s=2, c="blue")
+
+axe.set_xlim3d(xmin, xmax)
+axe.set_ylim3d(ymin, ymax)
+axe.set_zlim3d(zmin, zmax)
 
 ani = animation.FuncAnimation(fig, anim_scats, len(data), fargs=(data, scat), interval=50)
 
