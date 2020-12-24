b7d505f09166a4e331e92bb9ceddce93f95aa29a
Saeid Aliei <saeidaliei2019@gmail.com>
2020-12-24 17:16:39 +0100

    init interface

---
+++ directgrav.py
@@ -0,0 +1,23 @@
+# This is the interface to DirectGrav in python.
+
+import numpy as np
+
+class DirectGrav():
+    super.__init__(self, DirectGrav)
+
+    pass
+
+
+
+
+
+
+params = read_params(filename)
+
+grav = DirectGrav(params)
+
+grav.evolve()
+
+grav.animate()
+
+
